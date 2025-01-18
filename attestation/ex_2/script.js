0a6b8d6d3afa1b2ec0b28b67e2b2ebf7ba96632b
Alexandr Vinokurov <salispiligrim@yandex.ru>
2025-01-18 20:11:11 +0500

    ex 2 in attestation is complite

---
+++ attestation/ex_2/script.js
@@ -0,0 +1,23 @@
+async function saveUserData(user) {
+    try {
+        const response = await fetch("https://api.example.com/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        });
+        if (!response.ok) {
+            throw new Error("Error saving data");
+        }
+    } catch (error) {
+        return Promise.reject(error.message);
+    }
+}
+
+const user = {
+    name: "Misha",
+    email: "gus@gmail.com",
+    telephone: "89586345281",
+};
+saveUserData(user);
